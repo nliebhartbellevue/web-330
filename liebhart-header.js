3e7b86d56d12b74c235a32d5b44b681afbaa217b
liebhartdevelopment <nathaniel95975@gmail.com>
2019-08-06 11:31:01 -0500

    header file

---
+++ liebhart-header.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   June 9th 2019
+; Modified By: Nathaniel D. Liebhart
+; Description: Displays a formatted header
+;===========================================
+*/
+
+/**
+ * Params: firstName, lastName, assignment
+ * Response: output
+ * Description: Returns a well-formatted string header
+ */
+exports.display = function(firstName, lastName, assignment) {
+	let output =
+		'\n' +
+		firstName +
+		' ' +
+		lastName +
+		'\n' +
+		assignment +
+		'\nDate: ' +
+		new Date().toLocaleDateString('en-US');
+
+	return output;
+};
