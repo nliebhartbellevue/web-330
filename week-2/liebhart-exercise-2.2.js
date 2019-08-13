95f33e59f410437ed003eebafa1d6f10c2f52b72
liebhartdevelopment <nathaniel95975@gmail.com>
2019-08-13 15:06:54 -0500

    exercise 2.2

---
+++ week-2/liebhart-exercise-2.2.js
@@ -0,0 +1,51 @@
+/**
+ * Title: Exercise 2.2 - Prototypes
+ * Author: Nathaniel Liebhart
+ * Date: August 13th, 2019
+ * Description: Demonstrate an understanding of JavaScript Prototypes
+ */
+
+/**
+ * Params: firstName, lastName, assignment
+ * Response: output
+ * Description: Returns formatted header string
+ */
+const header = require("../liebhart-header");
+console.log(header("Nathaniel", "Liebhart", "Exercise 1.3") + "\n");
+
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ The person's full name is '<fullname>.'
+ The person's age is '<age>.'
+
+*/
+
+// start program
+
+// Create person object
+let person = {
+  getAge: function() {
+    return this.age;
+  }
+};
+
+// Create john object
+let john = Object.create(person, {
+  age: {
+    value: 42
+  },
+  fullname: {
+    value: "John Doe"
+  }
+});
+
+// Log results to console
+console.log(`The person's full name is ${john.fullname}`);
+console.log(`The person's age is ${john.getAge()}`);
+
+// end program
